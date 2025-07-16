730d3d912afd8246769a6b592b398e1633b16925
William Fagundes <william.fagundes.dev@gmail.com>
2025-07-16 00:43:16 -0300

    Versao Estável com Trades

--- menu.js
+++ menu.js
@@ -38,13 +38,13 @@ export function createHeader(auth, userData, parentElement, currentPage = '') {
                     </div>
                     
                     <!-- Botão Amigos -->
-                    <button class="flex items-center gap-2 bg-white/10 hover:bg-white/20 text-white font-bold px-3 py-2 rounded-lg transition-colors" title="Amigos">
+                    <button id="amigosbtn" class="flex items-center gap-2 bg-white/10 hover:bg-white/20 text-white font-bold px-3 py-2 rounded-lg transition-colors" title="Amigos">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/></svg>
                         <span class="hidden sm:inline text-sm">AMIGOS</span>
                     </button>
 
                     <!-- Botão Comprar Ouro -->
-                    <button class="flex items-center gap-2 bg-white/10 hover:bg-white/20 text-white font-bold px-3 py-2 rounded-lg transition-colors" title="Comprar Ouro">
+                    <button id="ourobtn" class="flex items-center gap-2 bg-white/10 hover:bg-white/20 text-white font-bold px-3 py-2 rounded-lg transition-colors" title="Comprar Ouro">
                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
                         <span class="hidden sm:inline text-sm">COMPRAR OURO</span>
                     </button>
@@ -66,6 +66,18 @@ export function createHeader(auth, userData, parentElement, currentPage = '') {
     if (logoutButton) {
         logoutButton.addEventListener('click', () => {
             signOut(auth).catch(error => console.error("Erro ao fazer logout:", error));
+        });
+    }
+    const amigosbtn = parentElement.querySelector('#amigosbtn');
+    if (amigosbtn) {
+        amigosbtn.addEventListener('click', () => {
+            window.location.href = './amigos.html'
+        });
+    }
+    const ourobtn = parentElement.querySelector('#ourobtn');
+    if (ourobtn) {
+        ourobtn.addEventListener('click', () => {
+            window.location.href = './ouro.html'
         });
     }
 }
