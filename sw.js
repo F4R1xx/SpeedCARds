4dbf538f95c877ca2c5cab3489a2bb0ef2f268ec
William Fagundes <william.fagundes.dev@gmail.com>
2025-12-16 22:44:06 -0300

    Volta de Versao para funcionar

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service Worker Mínimo
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Apenas repassa as requisições, não faz cache complexo
-    e.respondWith(fetch(e.request));
-});
